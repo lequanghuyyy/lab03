ab8eb5deeef1600e85e2b57b967e3de3fb172630
Your Name <you@example.com>
2024-10-09 14:39:01 +0700

    a

---
+++ .vs/Lab03/Lab03/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+     
+        }
+        Provider provider = new Provider();
+        public List<String> GetlistComboxBo()
+        {
+            string query = "exec GetThongTinSinhVien";
+            DataTable data = provider.ExcureQuery(query);
+            List<String> list = new List<String>();
+            foreach (DataRow item in data.Rows)
+            {
+             
+                list.Add(item["Quequan"].ToString());
+               
+            }
+            return list;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            string text = comboBoxQueQuan.SelectedItem.ToString();
+        
+            DataTable data = provider.ExcureQueryHavePara(text);
+            dataGridView1.DataSource = data;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxQueQuan.DataSource = GetlistComboxBo();
+           
+        }
+
+        private void comboBoxQueQuan_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string text = comboBoxQueQuan.SelectedItem.ToString();
+            dataGridView1.DataSource = text;
+            DataTable data = provider.ExcureQueryHavePara(text);
+            dataGridView1.DataSource = data;
+        }
+
+        private void buttonTatca_Click(object sender, EventArgs e)
+        {
+            string query = "exec GetThongTinSinhVien";
+            DataTable data = provider.ExcureQuery(query);
+            dataGridView1.DataSource= data;
+        }
+    }
+}
---
+++ .vs/Lab03/Lab03/Provider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    public class Provider
+    {
+       private string connectionString = "Data Source=607-20\\SQLEXPRESS;Initial Catalog=QLThucTap;Integrated Security=True";
+        public DataTable ExcureQuery(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            { 
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(connectionString, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query,connection);
+                sqlDataAdapter.Fill(data);
+            connection.Close();
+            }
+            return data;
+        }
+        public DataTable ExcureQueryHavePara(string para)
+        {
+            string query1 = "exec GetThongTinSinhVienTheoQueQuan @QueQuan";
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(query1, connection))
+                {
+                    cmd.Parameters.AddWithValue("@QueQuan", para); 
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                    sqlDataAdapter.Fill(data);
+                }
+            }
+            return data;
+        }
+    }
+   
+}
